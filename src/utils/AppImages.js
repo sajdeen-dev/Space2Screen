f1f2d859f8779bee37b67a2a59e68e61519d3599
sajdeen-dev <sayyedsajdeen.dev@gmail.com>
2025-07-08 22:55:45 +0530

    AppImages Added

---
+++ src/utils/AppImages.js
@@ -0,0 +1,5 @@
+export const AppImages ={
+    illustration01: require('../assets/Illustration01.png'),
+    illustration02: require('../assets/Illustration02.png'),
+    illustration03: require('../assets/Illustration03.png'),
+}
